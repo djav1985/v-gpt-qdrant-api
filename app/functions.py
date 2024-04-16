2f84ac3da4fe54ae868c297c5595e66752757806
Nikolai X. Shadeauxs <services@vontainment.com>
2024-04-16 01:21:21 -0400

    Update functions.py

--- app/functions.py
+++ app/functions.py
@@ -1,39 +1,13 @@
 import os
-import openai
-import uuid
-from qdrant_client import QdrantClient
 from scipy.spatial.distance import cosine
 from typing import List, Optional
 from pydantic import BaseModel
 from fastapi import HTTPException
 
 def load_configuration():
-    """Loads and returns application configuration details as a tuple."""
-    # Set OpenAI API Key globally for the library
-    openai_api_key = os.getenv("OPENAI_API_KEY")
-    openai.api_key = openai_api_key
-
-    # Initialize and return all configuration details along with QdrantClient
-    qdrant_client = QdrantClient(
-        url=f"http://{os.getenv('QDRANT_HOST', 'localhost')}:{int(os.getenv('QDRANT_PORT', 6333))}",
-        api_key=os.getenv("QDRANT_API_KEY")
-    )
-
     return (
         os.getenv("BASE_URL", "http://localhost"),
         os.getenv("API_KEY"),
-        os.getenv("QDRANT_HOST", "localhost"),
-        int(os.getenv("QDRANT_PORT", 6333)),
-        os.getenv("QDRANT_API_KEY"),
-        qdrant_client,
-        openai.api_key
-    )
-
-def get_qdrant_client():
-    """Create and return a QdrantClient instance."""
-    return QdrantClient(
-        url=f"http://{os.getenv('QDRANT_HOST', 'localhost')}:{int(os.getenv('QDRANT_PORT', 6333))}",
-        api_key=os.getenv("QDRANT_API_KEY")
     )
 
 def calculate_similarity_scores(text_entries, query_embedding):
@@ -62,7 +36,3 @@ def get_text_entries(qdrant_client, collection, keywords):
     except Exception as e:
         print(f"Failed to fetch or filter entries from Qdrant: {str(e)}")
         return []  # Return an empty list if there's an error
-
-def generate_unique_identifier():
-    # Generate a unique identifier using UUID4
-    return str(uuid.uuid4())
