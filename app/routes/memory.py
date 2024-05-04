82822d9c0f0fa5870c654e231a184873b5ae9ee0
Nikolai X. Shadeauxs <services@vontainment.com>
2024-05-03 22:21:26 -0400

    Update memory.py

--- app/routes/memory.py
+++ app/routes/memory.py
@@ -12,8 +12,8 @@ from qdrant_client import AsyncQdrantClient
 from qdrant_client.models import Distance, VectorParams, Filter, FieldCondition, PointStruct
 from fastembed import TextEmbedding
 
-from app.models import MemoryParams, SearchParams, CreateCollectionParams
-from app.dependencies import get_api_key, get_qdrant_client, get_embeddings_model
+from models import MemoryParams, SearchParams, CreateCollectionParams
+from dependencies import get_api_key, get_qdrant_client, get_embeddings_model
 
 router = APIRouter()
 
