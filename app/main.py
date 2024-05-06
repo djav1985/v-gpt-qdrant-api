3ea60cb64cd61921688c27de80b4860711f247e8
vontainment <services@vontainment.com>
2024-05-06 06:05:53 -0400

    up

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from starlette.responses import FileResponse
 from dependencies import limit_concurrency
 from routes.embeddings import embeddings_router
 from routes.memory import memory_router
+from routes.root import root_router
 
 # FastAPI Application Initialization
 app = FastAPI(
@@ -23,16 +24,7 @@ app.middleware('http')(limit_concurrency)
 # Including Routers
 app.include_router(memory_router)
 app.include_router(embeddings_router)
+app.include_router(root_router)
 
 # Mounting static files directory
 app.mount("/static", StaticFiles(directory="/app/public"), name="static")
-
-# Root endpoint - Serves the index.html file
-@app.get("/", include_in_schema=False)
-async def root():
-    return FileResponse("/app/public/index.html")
-
-# v1 endpoint - Serves the index.html file
-@app.get("/v1", include_in_schema=False)
-async def v1():
-    return FileResponse("/app/public/index.html")
