e724287a099e263197d87923bb49489d5f7a49b2
nikolai@vontainment.com <nikolai@vontainment.com>
2024-11-27 10:35:28 -0500

    modified:   app/dependencies.py

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,28 +1,29 @@
 # /dependencies.py
-
-# Importing standard libraries for operating system interaction and async functionality
 import os
 import asyncio
 import numpy as np
+
+from typing import Any, Tuple, Optional
+
 from fastapi import HTTPException, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
 from fastembed import TextEmbedding
 from qdrant_client import AsyncQdrantClient
 from sklearn.neighbors import NearestNeighbors
 
-
 # Singleton class to manage a single instance of TextEmbedding
 class SingletonTextEmbedding:
-    _instance = None
+    _instance: Optional[TextEmbedding] = None
 
     @classmethod
-    def get_instance(cls):
+    def get_instance(cls) -> TextEmbedding:
         if cls._instance is None:
             raise Exception("SingletonTextEmbedding has not been initialized")
         return cls._instance
 
     @classmethod
-    async def initialize(cls):
+    async def initialize(cls) -> None:
         if cls._instance is None:
             cls._instance = TextEmbedding(
                 model_name=os.getenv("LOCAL_MODEL"),
@@ -31,38 +32,33 @@ class SingletonTextEmbedding:
                 threads=3,
             )
 
-
 # Function to initialize text embedding at app startup
-async def initialize_text_embedding():
+async def initialize_text_embedding() -> None:
     await SingletonTextEmbedding.initialize()
 
-
 # Dependency to get embeddings model
-def get_embeddings_model():
+def get_embeddings_model() -> TextEmbedding:
     return SingletonTextEmbedding.get_instance()
 
-
 # Function to create Qdrant client
-async def create_qdrant_client():
+async def create_qdrant_client() -> AsyncQdrantClient:
     return AsyncQdrantClient(
         url=os.getenv("QDRANT_HOST", "http://qdrant:6333"),
         api_key=os.getenv("QDRANT_API_KEY"),
     )
 
-
 # This function checks if the provided API key is valid or not
 async def get_api_key(
     credentials: HTTPAuthorizationCredentials = Depends(HTTPBearer(auto_error=False)),
-):
+) -> Optional[str]:
     if os.getenv("MEMORIES_API_KEY") and (
         not credentials or credentials.credentials != os.getenv("MEMORIES_API_KEY")
     ):
         raise HTTPException(status_code=403, detail="Invalid or missing API key")
     return credentials.credentials if credentials else None
 
-
 # Function to compute dynamic `eps` and `min_samples` for DBSCAN
-def compute_dynamic_dbscan_params(vectors, k=5):
+def compute_dynamic_dbscan_params(vectors: np.ndarray, k: int = 5) -> Tuple[float, int]:
     nearest_neighbors = NearestNeighbors(n_neighbors=k)
     neighbors = nearest_neighbors.fit(vectors)
     distances, indices = neighbors.kneighbors(vectors)
